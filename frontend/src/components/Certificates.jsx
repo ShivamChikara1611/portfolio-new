722c850ec6a30a6058eb875960733e214c454cc0
shivam chikara <s.chikara6885@gmail.com>
2025-09-21 23:13:14 +0530

    added the heading of certifications

--- frontend/src/components/Certificates.jsx
+++ frontend/src/components/Certificates.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { useContext } from "react";
+import { AppContext } from "../context/AppContext";
 
-const Certificates = () => {
+export default function Certificates() {
+    const { language } = useContext(AppContext);
+
+    const texts = {
+        en: {
+        },
+        jp: {
+        },
+    };
+
     return (
-        <div></div>
-    )
+        <section
+            id="certificates"
+            className="mx-auto mt-16 text-white overflow-hidden max-w-[1200px]"
+        >
+            <h2 className="text-white text-center mb-5 font-black text-4xl sm:text-5xl md:text-6xl">
+                {language === "en" ? "Certifications" : "認証"}
+            </h2>
+        </section>
+    );
 }
-
-export default Certificates
